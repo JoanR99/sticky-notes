34b1c05838d835e14cab2e7408432655f9ee3cd3
Joan Romero <romerojoan1999@gmail.com>
2022-10-14 11:20:42 -0400

    change cors

--- src/app.ts
+++ src/app.ts
@@ -38,18 +38,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../frontend/dist')));
-}
-
 app.use('/api/notes', notesRouter);
 app.use('/api/users', userRouter);
 app.use('/api/colors', colorsRouter);
 app.use('/api/auth', authRouter);
-
-app.get('*', (_req, res) => {
-	res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
 
 app.use(errorHandler);
 
--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -4,7 +4,7 @@ import allowedOrigins from './allowedOrigins';
 const corsOptions: CorsOptions = {
 	origin: (origin, callback) => {
 		if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-			callback(null, true);
+			callback(null, origin);
 		} else {
 			callback(new Error('Not allowed by CORS'));
 		}
